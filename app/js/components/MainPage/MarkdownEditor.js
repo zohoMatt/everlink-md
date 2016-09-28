6772cce06f39b7b95275f1957a97dc0abaaa677d
zohoFrank <zuoh14@mails.tsinghua.edu.cn>
2016-09-28 20:33:56 +0800

    feat: Replace textarea with codemirror

--- app/js/components/MainPage/MarkdownEditor.js
+++ app/js/components/MainPage/MarkdownEditor.js
@@ -7,25 +7,43 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import { compileMarkdownAction } from '../../model/actions/compileMarkdownAction';
+// CodeMirror add-ons
+const Codemirror = require('react-codemirror');
+require('codemirror/mode/markdown/markdown');
 
 @connect(() => {
 	return {};
 })
 export default class MarkdownEditor extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			code: '# EverlinkMD'
+		}
+	}
 
     render() {
+    	const codeMirrorOptions = {
+			lineNumbers: true,
+			mode: 'markdown'
+		};
         return (
         	<div id="md-editor">
-				<textarea name="md-raw" id="md-content"
-						  onChange={this.dispatchManager.bind(this)}>
-				</textarea>
+				{/*<textarea name="md-raw" id="md-content"*/}
+						  {/*onChange={this.dispatchManager.bind(this)}*/}
+						  {/*onBlur={this.blurHandler.bind(this)}># EverlinkMD</textarea>*/}
+				<Codemirror value={this.state.code}
+							onChange={this.updateCode.bind(this)}
+							options={codeMirrorOptions} />
 			</div>
 		);
     }
 
     /*****************************************/
-    dispatchManager(event) {
-    	const markdown = event.target.value;
-		this.props.dispatch(compileMarkdownAction(markdown));
+    updateCode(newCode) {
+    	this.setState({
+    		code: newCode
+		});
+		this.props.dispatch(compileMarkdownAction(newCode));
 	}
 }
