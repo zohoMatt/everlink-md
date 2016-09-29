d031739cb405a343f4c6ad6f5c0b9d495033777f
zohoFrank <zuoh14@mails.tsinghua.edu.cn>
2016-09-30 00:18:38 +0800

    feat: ActionPanel buttons

---
+++ app/js/components/MainPage/ActionPanel/PreferenceSettingButton.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Zoho on 16/9/29.
+ */
+import React from 'react';
+
+export default class PreferenceSettingButton extends React.Component {
+
+    render() {
+        return (
+			<div className="panel-button">
+				<i className="fa fa-cog" aria-hidden="true"></i>
+			</div>
+		);
+    }
+}
---
+++ app/js/components/MainPage/ActionPanel/SyncEvernoteButton.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Zoho on 16/9/29.
+ */
+import React from 'react';
+
+export default class SyncEvernoteButton extends React.Component {
+
+	render() {
+		return (
+			<div className="panel-button">
+				<i className="fa fa-refresh" aria-hidden="true"></i>
+			</div>
+		);
+	}
+}
