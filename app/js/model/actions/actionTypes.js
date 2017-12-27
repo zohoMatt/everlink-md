3f64ee0a40461593af9d53b8b98c5058d4ecb2e5
zohoMatt <zuoh14@mails.tsinghua.edu.cn>
2017-12-27 17:04:28 +0800

    Delete all and refactor it.

--- app/js/model/actions/actionTypes.js
+++
@@ -1,4 +0,0 @@
-/**
- * Created by Zoho on 16/9/28.
- */
-export const COMPILE_MARKDOWN = 'COMPILE_MARKDOWN';
--- app/js/model/store/developerSpecifiedData.js
+++
@@ -1,8 +0,0 @@
-/**
- * Create by Zoho on 17/7/20
- */
-
-/** Default store for redux **/
-export const DEFAULT_STORE = {
-	textContent: '# EverlinkMD'
-};
