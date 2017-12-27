8c4d66cebd78d36b5e736d234fea207ca2f3f84e
zohoMatt <zuoh14@mails.tsinghua.edu.cn>
2017-12-27 16:58:58 +0800

    feat: MaiPage styles

--- app/containers/HomePage.js
+++ app/containers/HomePage.js
@@ -1,12 +1,11 @@
 // @flow
 import React, { Component } from 'react';
-import Home from '../components/Home';
 import MainPage from '../components/MainPage';
 
 export default class HomePage extends Component {
   render() {
     return (
-      <Home />
+      <MainPage />
     );
   }
 }
