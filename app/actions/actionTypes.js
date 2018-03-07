9f048bb3bbc9bca0394937dad76ba46866e61e22
zohoMatt <zuoh14@mails.tsinghua.edu.cn>
2018-03-07 18:51:44 +0800

    feat: Connect modal state to redux.

--- app/actions/actionTypes.js
+++ app/actions/actionTypes.js
@@ -3,8 +3,21 @@
  * Created by Zoho on 16/9/28.
  */
 
+type actionTypes = {
+  +type: string,
+  payload?: any
+};
+
 /** ****************** CodeMirror *******************/
 export const COMPILE_MARKDOWN: string = 'COMPILE_MARKDOWN';
+
+/******************** Modal *******************/
+export const TOGGLE_MODAL_VISIBILITY: string = 'TOGGLE_MODAL_VISIBILITY';
+
+export const toggleModal = state => (dispatch: (action: actionTypes) => void) => dispatch({
+  type: TOGGLE_MODAL_VISIBILITY,
+  payload: state
+});
 
 export default {
   COMPILE_MARKDOWN
--- app/reducers/toggleModal.js
+++ app/reducers/toggleModal.js
@@ -2,9 +2,13 @@
  * Created by MattZo on 2018/3/7
  */
 import INITIAL_STATE from '../store/initial';
+import { TOGGLE_MODAL_VISIBILITY } from 'actions/actionTypes';
 
 export const toggleModal = (state = INITIAL_STATE.modalVisible, action) => {
+  console.log(action);
   switch (action.type) {
+    case TOGGLE_MODAL_VISIBILITY:
+      return action.payload
     default:
       return state;
   }
