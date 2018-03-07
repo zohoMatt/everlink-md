389a0b4711a6e36aaf49049eed8ea4aa165ab9ba
zohoMatt <zuoh14@mails.tsinghua.edu.cn>
2018-03-07 14:23:17 +0800

    feat: Change flow type for actions.

--- app/actions/actionTypes.js
+++ app/actions/actionTypes.js
@@ -3,12 +3,8 @@
  * Created by Zoho on 16/9/28.
  */
 
-export type actionType = {
-  +type: string
-};
-
 /** ****************** CodeMirror *******************/
-export const COMPILE_MARKDOWN = 'COMPILE_MARKDOWN';
+export const COMPILE_MARKDOWN: string = 'COMPILE_MARKDOWN';
 
 export default {
   COMPILE_MARKDOWN
