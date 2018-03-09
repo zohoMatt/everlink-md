75f7480be9a84b5c89e8ecd204a5af800742dd1d
zohoMatt <zuoh14@mails.tsinghua.edu.cn>
2018-03-09 17:12:15 +0800

    feat: Starting to develop LinkModal

--- app/components/MainPage/modals/AddLinkModal.js
+++ app/components/MainPage/modals/AddLinkModal.js
@@ -7,10 +7,12 @@ import React from 'react';
 import ModalContainer from 'components/MainPage/ModalContainer';
 import ButtonTypes from 'utils/fontMap';
 
+import styles from './AddLinkModal.scss';
+
 const AddLinkModal = () => {
   return (
     <ModalContainer typeName={ButtonTypes.InsertLinkButton}>
-      <h1>Here to insert a link</h1>
+      <div className={styles.title}>Insert a link into context</div>
     </ModalContainer>
   );
 };
