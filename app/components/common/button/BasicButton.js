9636c6a384d2e000f71bbe2cd7661a58a79c670a
zohoMatt <zuoh14@mails.tsinghua.edu.cn>
2018-03-22 17:05:29 +0800

    feat: Make BasicButton stateless.

--- app/components/common/button/BasicButton.js
+++ app/components/common/button/BasicButton.js
@@ -1,52 +1,22 @@
 /**
  * Created by MattZo on 2018/3/22
  */
-// @flow
 import React from 'react';
 
 import styles from './BasicButton.scss';
 
-type buttonState = {
-  status: 'hover' | 'default' | 'pressed'
+const BasicButton = ({ width, text, disabled }) => {
+  return <button
+    className={styles.normalBtn}
+    style={{ width }}
+    disabled={disabled}
+  >{ text }</button>;
 };
 
-class BasicButton extends React.Component {
-  props: {
-    width: string,
-    text: string,
-    disabled: boolean
-  };
-
-  static defaultProps = {
-    width: '150px',
-    text: 'A lonely button',
-    disabled: false
-  };
-
-  state: buttonState = {
-    status: 'default'
-  };
-
-  switchStatus(status) {
-    this.setState({
-      ...this.state, status
-    });
-  }
-
-  render() {
-    const { width, text, disabled } = this.props;
-    const switchBtn = status => () => this.switchStatus(status);    // Wrapper for method
-
-    return <button
-      className={styles.normalBtn}
-      style={{ width }}
-      disabled={disabled}
-      onMouseOver={switchBtn('hover')}
-      onMouseLeave={switchBtn('default')}
-      onMouseDown={switchBtn('pressed')}
-      onMouseUp={switchBtn('hover')}
-    >{ text }</button>;
-  }
-}
+BasicButton.defaultProps = {
+  width: '150px',
+  text: 'A lonely button',
+  disabled: false
+};
 
 export default BasicButton;
