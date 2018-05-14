2b81e837cb9d4cb89b5e503b31b6d656fc1e7667
zohoMatt <zuoh14@mails.tsinghua.edu.cn>
2018-05-14 11:39:10 +0800

    feat: Refactor stateless component into a class.

--- app/components/MainPage/ModalContainer.js
+++ app/components/MainPage/ModalContainer.js
@@ -1,22 +1,44 @@
 /**
  * Created by MattZo on 2018/3/7
  */
+//@flow
 import React from 'react';
+import type {Node, Element} from 'react';
 
 import toToggleModal from 'containers/HOC/toToggleModal';
 import styles from './ModalContainer.scss';
 
-const ModalContainer = ({ children, toggleModal, posStyles }) => (
-  <div className={styles.modalBackground}>
-    <div className={styles.modalContainer} style={posStyles}>
-      <div role="button" className={styles.closeBtn} onClick={() => toggleModal(false)}>
-        <svg className={styles.icon} viewBox="0 0 16 16">
-          <path d="M15.854 12.854c-0-0-0-0-0-0l-4.854-4.854 4.854-4.854c0-0 0-0 0-0 0.052-0.052 0.090-0.113 0.114-0.178 0.066-0.178 0.028-0.386-0.114-0.529l-2.293-2.293c-0.143-0.143-0.351-0.181-0.529-0.114-0.065 0.024-0.126 0.062-0.178 0.114 0 0-0 0-0 0l-4.854 4.854-4.854-4.854c-0-0-0-0-0-0-0.052-0.052-0.113-0.090-0.178-0.114-0.178-0.066-0.386-0.029-0.529 0.114l-2.293 2.293c-0.143 0.143-0.181 0.351-0.114 0.529 0.024 0.065 0.062 0.126 0.114 0.178 0 0 0 0 0 0l4.854 4.854-4.854 4.854c-0 0-0 0-0 0-0.052 0.052-0.090 0.113-0.114 0.178-0.066 0.178-0.029 0.386 0.114 0.529l2.293 2.293c0.143 0.143 0.351 0.181 0.529 0.114 0.065-0.024 0.126-0.062 0.178-0.114 0-0 0-0 0-0l4.854-4.854 4.854 4.854c0 0 0 0 0 0 0.052 0.052 0.113 0.090 0.178 0.114 0.178 0.066 0.386 0.029 0.529-0.114l2.293-2.293c0.143-0.143 0.181-0.351 0.114-0.529-0.024-0.065-0.062-0.126-0.114-0.178z" />
-        </svg>
+type Props = {
+  children: Node,
+  toggleModal: Element => void,
+  posStyles: object
+};
+
+type State = {
+}
+
+class ModalContainer extends React.Component<Props, State> {
+  static defaultProps = {
+    children: null,
+    toggleModal: () => null,
+    posStyles: {}
+  };
+
+  render() {
+    const { children, toggleModal, posStyles } = this.props;
+    return (
+      <div className={styles.modalBackground}>
+        <div className={styles.modalContainer} style={posStyles}>
+          <div role="button" className={styles.closeBtn} onClick={() => toggleModal(false)}>
+            <svg className={styles.icon} viewBox="0 0 16 16">
+              <path d="M15.854 12.854c-0-0-0-0-0-0l-4.854-4.854 4.854-4.854c0-0 0-0 0-0 0.052-0.052 0.090-0.113 0.114-0.178 0.066-0.178 0.028-0.386-0.114-0.529l-2.293-2.293c-0.143-0.143-0.351-0.181-0.529-0.114-0.065 0.024-0.126 0.062-0.178 0.114 0 0-0 0-0 0l-4.854 4.854-4.854-4.854c-0-0-0-0-0-0-0.052-0.052-0.113-0.090-0.178-0.114-0.178-0.066-0.386-0.029-0.529 0.114l-2.293 2.293c-0.143 0.143-0.181 0.351-0.114 0.529 0.024 0.065 0.062 0.126 0.114 0.178 0 0 0 0 0 0l4.854 4.854-4.854 4.854c-0 0-0 0-0 0-0.052 0.052-0.090 0.113-0.114 0.178-0.066 0.178-0.029 0.386 0.114 0.529l2.293 2.293c0.143 0.143 0.351 0.181 0.529 0.114 0.065-0.024 0.126-0.062 0.178-0.114 0-0 0-0 0-0l4.854-4.854 4.854 4.854c0 0 0 0 0 0 0.052 0.052 0.113 0.090 0.178 0.114 0.178 0.066 0.386 0.029 0.529-0.114l2.293-2.293c0.143-0.143 0.181-0.351 0.114-0.529-0.024-0.065-0.062-0.126-0.114-0.178z" />
+            </svg>
+          </div>
+          { children }
+        </div>
       </div>
-      { children }
-    </div>
-  </div>
     );
+  }
+}
 
 export default toToggleModal(ModalContainer);
